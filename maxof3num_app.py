fc62836a7ac4453e5225fefefda3b821a8df1904
penumallipratap <167686303+penumallipratap@users.noreply.github.com>
2024-04-21 18:27:40 +0530

    Update maxof3num_app.py

--- maxof3num_app.py
+++ maxof3num_app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 st.write('find max of 3 given numbers')
 
-st.read(number1 = float(input("Enter the first number: ")))
-number2 = float(input("Enter the second number: "))
-number3 = float(input("Enter the third number: "))
+number1 = st.number_input("Enter the first number:")
+number2 = st.number_input("Enter the second number:")
+number3 = st.number_input("Enter the third number: ")
 
-print("You entered:", number1, number2, number3)
+st.write("You entered:", number1, number2, number3)
 
-largest_number = max(number1, number2, number3)
-print("The largest number is:", largest_number)
+largest_number = st.max(number1, number2, number3)
+st.write("The largest number is:", largest_number)
