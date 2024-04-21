1f6af13ff1b41f147f73f913d7e6eb6be497f2e0
penumallipratap <167686303+penumallipratap@users.noreply.github.com>
2024-04-21 18:08:09 +0530

    Create maxof3num_app.py

---
+++ maxof3num_app.py
@@ -0,0 +1,11 @@
+import stremlit as st
+st.write('find max of 3 given numbers')
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+number3 = float(input("Enter the third number: "))
+
+print("You entered:", number1, number2, number3)
+
+largest_number = max(number1, number2, number3)
+print("The largest number is:", largest_number)
