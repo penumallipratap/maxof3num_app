6c077e1a4aeb6e2cb8a492ce57cfbbdf22445b6d
penumallipratap <167686303+penumallipratap@users.noreply.github.com>
2024-04-21 18:36:42 +0530

    Update maxof3num_app.py

--- maxof3num_app.py
+++ maxof3num_app.py
@@ -9,6 +9,6 @@ st.write("You entered:", number1, number2, number3)
 
 largest_number = max(number1, number2, number3)
 if st.button("(Calculate)"):  # Add a button to trigger calculation on user click
-  st.write("(The maximum number among the entered numbers is", largest_number, ")")
+  #st.write("(The maximum number among the entered numbers is", largest_number, ")")
 
-#st.write("The largest number is:", largest_number)
+  st.write("The largest number is:", largest_number)
